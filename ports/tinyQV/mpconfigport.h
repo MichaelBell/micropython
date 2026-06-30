410e6e5a1ef6fd75b31e6a02d1c6e6d332a22461
Mike Bell <mdb036@gmail.com>
2026-06-30 23:28:21 +0100

    Identify

--- ports/tinyQV/mpconfigport.h
+++ ports/tinyQV/mpconfigport.h
@@ -29,7 +29,7 @@ typedef long mp_off_t;
 // We need to provide a declaration/definition of alloca()
 #include <alloca.h>
 
-#define MICROPY_HW_BOARD_NAME "ttsky25a"
+#define MICROPY_HW_BOARD_NAME "ws01"
 #define MICROPY_HW_MCU_NAME "tinyQV"
 
 #define MICROPY_HEAP_SIZE      (1 * 1024 * 1024)    // heap size 1MB
